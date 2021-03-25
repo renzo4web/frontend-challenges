a9725a897e072769177fc3d5053a42da10583a90
Renzo Barrios <w3renzo@gmail.com>
2021-03-25 12:14:02 -0300

    Nav Desktop in progress

--- blogr-landing-page-main/index.js
+++ blogr-landing-page-main/index.js
@@ -1,8 +1,8 @@
 const iconHamburguer = document.getElementById('icon-hamburger');
 const navMobile = document.querySelector('.nav__mobile');
-const navListParent = document.querySelectorAll('.header-list > li');
+let navListParent = document.querySelectorAll('.header-list > li');
 const navListChildren = document.querySelectorAll('.header-list ul');
-const list = document.querySelectorAll('.navs');
+let list = document.querySelectorAll('.nav__mobile .navs');
 
 const handleClickHambumger = () => {
   navMobile.classList.toggle('visible');
@@ -11,35 +11,57 @@ const handleClickHambumger = () => {
     : './images/icon-hamburger.svg';
 };
 
-iconHamburguer.addEventListener('click', handleClickHambumger);
-
-const hideList = (clicked) => {
-
-  list.forEach((li, j) => {
+const hideList = (clicked,parent,lis) => {
+  console.log('yy');
+  lis.forEach((li, j) => {
     if (clicked === li) {
       li.classList.toggle('show');
-      console.log(navListParent[j + 1]);
-      navListParent[j].classList.remove('hidden');
+      console.log(parent[j + 1]);
+      parent[j].classList.remove('hidden');
       if (j > 0) {
         for (let i = j; i >= 0; i--) {
-          navListParent[i].classList.remove('hidden');
+          parent[i].classList.remove('hidden');
         }
       }
     } else {
-      navListParent[j].classList.add('hidden');
+      parent[j].classList.add('hidden');
       li.classList.remove('show');
     }
   });
 };
 
-navListParent.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    const currentList = document.querySelector(
-      `.navs.${e.target.textContent}__ul`
-    );
-    hideList(currentList);
-   if (Array.from(list).every(li => !li.classList.contains("show"))){
-     navListParent.forEach(li=> li.classList.remove("hidden"))
-   }
+if (iconHamburguer.style.display !== 'none') {
+  console.log('daskljd');
+  iconHamburguer.addEventListener('click', handleClickHambumger);
+  navListParent.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const currentList = document.querySelector(
+        `.navs.${e.target.textContent}__ul`
+      );
+      hideList(currentList,navListParent,list);
+      if (Array.from(list).every((li) => !li.classList.contains('show'))) {
+        navListParent.forEach((li) => li.classList.remove('hidden'));
+      }
+    });
   });
-});
+}
+
+  /*  Desktop Nav */
+  const navDesktop = document.querySelector('.nav__desktop');
+  const navListParentDestop = document.querySelectorAll(
+    '.nav__desktop .header-list > li'
+  );
+  const listDestop = document.querySelectorAll('.nav__desktop .navs');
+  navListParentDestop.forEach((heading) => {
+    heading.addEventListener('mouseover', (e) => {
+      const currentList = document.querySelector(
+        `.nav__desktop .navs.${e.target.textContent}__ul`
+      );
+      console.log(e.target);
+      hideList(currentList,navListParentDestop,listDestop);
+      if (Array.from(listDestop).every((li) => !li.classList.contains('show'))) {
+        navListParent.forEach((li) => li.classList.remove('hidden'));
+      }
+    });
+  });
+
